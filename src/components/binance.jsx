79bf1b156550fce096783d9c0f7f24444508cd05
Atharvakumkar <kumkaratharva8@gmail.com>
2026-02-15 22:10:55 +0530

    Created Component Binance
    Description about what is Binance

---
+++ src/components/binance.jsx
@@ -0,0 +1,10 @@
+function Binance() {
+    return (
+        <div className="bg-zinc-900">
+            <h1 className="px-20 pt-15 font-heading text-white text-[32px]">What is <span className="text-amber-300">Binance?</span></h1>
+            <p className="px-20 font-heading text-white text-[18px]">Binance is a global cryptocurrency exchange platform that enables users to buy, sell, trade, and store a wide range of digital assets. The platform provides tools and services for both beginner and experienced investors, including spot trading, derivatives trading, staking, and educational resources aimed at supporting users in navigating the cryptocurrency ecosystem.</p>
+        </div>
+    );
+}
+
+export default Binance;
