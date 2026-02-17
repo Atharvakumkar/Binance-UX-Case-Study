d5cee1bd82327f24bd7767f72cf6b45d912dae3f
Atharvakumkar <kumkaratharva8@gmail.com>
2026-02-17 22:36:07 +0530

    Centered the heading

--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,9 +1,13 @@
 function Heading() {
     return (
         <div className="bg-zinc-900">
-            
-            <h1 className="font-heading px-20 pt-10 text-[48px] text-white font-bold"><span className="text-amber-300">Binance</span> UX Case Study</h1>
-            <h2 className="font-heading px-20 text-[20px] text-white">By <span className="text-purple-500">Atharva Kumkar</span></h2>
+            <h1 className="text-center font-heading px-20 pt-10 text-[48px] text-white font-bold">
+                <span className="text-amber-300">Binance</span> UX Case Study
+            </h1>
+
+            <h2 className="text-center font-heading text-[20px] text-white">
+                By <span className="text-purple-500">Atharva Kumkar</span>
+            </h2>
         </div>
     );
 }
