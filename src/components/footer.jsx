6a7a68c03fedac7dbeaa8a84729364d33249974c
Atharvakumkar <kumkaratharva8@gmail.com>
2026-02-19 22:02:12 +0530

    Created component Footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <div className="pt-13">
+        <div className="bg-zinc-800 pt-8">
+            <h1 className="flex justify-center font-heading text-[18px] text-white opacity-80">Thanks for reading this Case Study</h1>
+            <h2 className="flex justify-center font-heading text-[18px] text-white opacity-80">Best wishes from Atharva!</h2>
+        </div>
+        </div>
+    );
+}
+
+export default Footer;
